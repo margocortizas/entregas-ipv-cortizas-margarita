d0b9000e6893c01f7f70a9ca84fbbb6e66194710
Margo <margarita.cortizas@gmail.com>
2024-03-26 01:48:13 -0300

    Entrego el desafío 4

---
+++ entrega-4-cortizas-margarita/entities/player/Cannon.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+onready var fire_position : Position2D = $FirePosition
+
+export (PackedScene) var projectile_scene : PackedScene
+
+var projectile_container : Node 
+
+
+
+func fire():
+	var projectile_instance : Projectile = projectile_scene.instance()
+	projectile_container.add_child(projectile_instance)
+	projectile_instance.set_starting_values(fire_position.global_position, (fire_position.global_position - global_position).normalized())
+	projectile_instance.connect("delete_requested",self, "on_projectile_delete_requested")
+	
+func on_projectile_delete_requested(projectile):
+	projectile_container.remove_child(projectile)
+	projectile.queue_free()
+	
