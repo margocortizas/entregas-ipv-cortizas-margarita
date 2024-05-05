9c0487b0bf0e16576fa3d5e5a799c0082892ff2b
Margo <margarita.cortizas@gmail.com>
2024-05-05 20:06:55 -0300

    Subo desafio 8

---
+++ entrega-8-cortizas-margarita/src/game/ui/PauseMenu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+onready var options_menu = $OptionsMenu
+
+## Menú de pausa genérico, abierto utilizando la acción "pause_menu"
+## (por default la tecla Esc).
+
+signal return_selected()
+
+func _ready() -> void:
+	hide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("pause_menu") && !options_menu.visible:
+		visible = !visible
+		get_tree().paused = visible
+
+
+func _on_ResumeButton_pressed() -> void:
+	hide()
+	get_tree().paused = false
+
+
+func _on_ReturnButton_pressed():
+	emit_signal("return_selected")
